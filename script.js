3fc55e2714901d6714178c4723d9abdc9011fad4
Matt Bower <MB2@joness-imac.lan>
2020-02-27 14:43:02 -0800

    1 btc = 777.00 works

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
     .then(res => res.json())
     .then(data => {
-      // console.log('data ' + JSON.stringify(data));
-      const moneyRate = data.rates[currencyTwo];
+      console.log('data: ' + JSON.stringify(data));
+      const moneyRate = data.rates[currencyOne];
+      console.log('fiat moneyRate: ', moneyRate);
       rate.innerText = `1 ${currencyOne} = ${moneyRate} ${currencyTwo}`;
       amountEl_two.value = (amountEl_one.value * moneyRate).toFixed(2);
     })
 }
-
 
 // fetch cryptocurrency rates
 function fetchCrypto() {
@@ -41,8 +41,17 @@ function fetchCrypto() {
   request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apiKey.key)
     .then((res) => {
       var data = JSON.parse(res.target.response);
-      console.log(data.data)
-      console.log(data.data[0].quote.USD.price);
+      const btc = data.data[0].quote.USD.price;
+      const eth = data.data[1].quote.USD.price;
+      const moneyRate = btc;
+      const currencyOne = currencyEl_one.value;
+      const currencyTwo = currencyEl_two.value;
+      console.log('data.data: ', data.data)
+      console.log('btc: ', data.data[0].quote.USD.price);
+      console.log('eth', data.data[1].quote.USD.price);
+      console.log('crypto moneyRate: ', moneyRate);
+      rate.innerText = `1 ${currencyTwo} = ${moneyRate} ${currencyOne}`;
+      amountEl_one.value = (amountEl_two.value * moneyRate).toFixed(2);
     }).catch((err) => err);
   function request(method, url) {
     return new Promise((resolve, reject) => {
@@ -54,5 +63,6 @@ function fetchCrypto() {
     });
   }
 }
+
 fetchCrypto();
 calculate();
