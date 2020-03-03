5a4dd20ec64b626df5b688e4d88610a70c7f3c2f
Matt Bower <MB2@joness-imac.lan>
2020-03-03 08:05:22 -0800

    loop through data

--- script.js
+++ script.js
@@ -25,12 +25,13 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
     .then(res => res.json())
     .then(data => {
-      console.log('data: ' + JSON.stringify(data));
+      console.log('fiat data: ' + JSON.stringify(data));
       const moneyRate = data.rates[currencyOne];
       console.log('fiat moneyRate: ', moneyRate);
       rate.innerText = `1 ${currencyOne} = ${moneyRate} ${currencyTwo}`;
       amountEl_two.value = (amountEl_one.value * moneyRate).toFixed(2);
     })
+    // fetchCrypto();
 }
 
 // fetch cryptocurrency rates
@@ -41,17 +42,24 @@ function fetchCrypto() {
   request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apiKey.key)
     .then((res) => {
       var data = JSON.parse(res.target.response);
-      const btc = data.data[0].quote.USD.price;
-      const eth = data.data[1].quote.USD.price;
-      const moneyRate = btc;
+      console.log('data.data: ', data.data);
+      // const btc = data.data[0].quote.USD.price;
+      // const eth = data.data[1].quote.USD.price;
       const currencyOne = currencyEl_one.value;
       const currencyTwo = currencyEl_two.value;
-      console.log('data.data: ', data.data)
-      console.log('btc: ', data.data[0].quote.USD.price);
-      console.log('eth', data.data[1].quote.USD.price);
-      console.log('crypto moneyRate: ', moneyRate);
-      rate.innerText = `1 ${currencyTwo} = ${moneyRate} ${currencyOne}`;
-      amountEl_one.value = (amountEl_two.value * moneyRate).toFixed(2);
+      for (var i = 0; i < data.data.length; i++) {
+        const price = data.data[i].quote.USD.price;
+        const symbol = data.data[i].symbol;
+        console.log('crypto price[i]: ', price);
+        console.log('crypto symbol: ', symbol);
+        console.log('');
+        if (currencyTwo === symbol) {
+          rate.innerText = `1 ${currencyTwo} = ${price} ${currencyOne}`;
+          amountEl_one.value = (amountEl_two.value * price).toFixed(2);
+        } else {
+          
+        }
+      }
     }).catch((err) => err);
   function request(method, url) {
     return new Promise((resolve, reject) => {
@@ -64,5 +72,6 @@ function fetchCrypto() {
   }
 }
 
-fetchCrypto();
 calculate();
+fetchCrypto();
+
