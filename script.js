d7b7b7fd5adc973074ff179bf1fb4b3c2bc22609
Matthew Bower <mttbwr91@gmail.com>
2020-03-10 20:17:19 -0700

    refactor

--- script.js
+++ script.js
@@ -4,43 +4,71 @@ const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 const rate = document.getElementById('rate');
+// const swap = document.getElementById('swap');
+// swap.addEventListener('click', () => {
+//   const temp = currencyEl_one.value;
+//   currencyEl_one.value = currencyEl_two.value;
+//   currencyEl_two.value = temp;
+//   calculate();
+// });
 
 currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
+amountEl_one.addEventListener('input', function(){calculate("one")});
 currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', function(){calculate("two")});
 
 // fetch exchange rates and update DOM
-function calculate() {
+function calculate(num) {
+
   const currencyOne = currencyEl_one.value;
   const currencyTwo = currencyEl_two.value;
+  console.log(" ");
+  console.log(" ");
+  console.log("==============================================");
+  console.log("num: " + num);
+  console.log("currencyOne: " + currencyOne);
+  console.log("currencyTwo: " + currencyTwo);
   fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
     .then(res => res.json())
     .then(data => {
       // console.log('fiat data.rates: ', data.rates);
       for (const [key, value] of Object.entries(data.rates)) {
-        // console.log(`key: ${key}, value: ${value}`);
+       //console.log(`key: ${key}, value: ${value}`);
         if (currencyOne === key) {
+          //console.log("Inside if statement");
+          //console.log("currencyOne === key " + currencyOne === key);
+          //let currRate = 
           rate.innerText = `1 ${currencyTwo} = ${value} ${currencyOne}`;
-          amountEl_one.value = (amountEl_two.value * value).toFixed(2);
-          amountEl_two.value = (amountEl_one.value * value).toFixed(2);
+          // Calcualte Here
+          console.log("value: " + value);
+          console.log("amount one raw: " + amountEl_one.value );
+          console.log("amount two raw: " + amountEl_two.value );
+          
+          // TODO
+          //amountEl_one.value = (amountEl_two.value * value).toFixed(2);
+          //amountEl_two.value = (amountEl_one.value * value).toFixed(2);
         }
       }
     })
-    // calling this allows the rate element work
-    fetchCMC();
-    fetchCryptoCompare()
+    
+    // TODO
+    fetchCMC(num);
+    console.log(" ");
+    console.log(" ");
+    console.log(" ");
+    console.log(" ");
+    //fetchMT()
 }
 
 // fetch cryptocurrency rates
-function fetchCMC() {
+function fetchCMC(num) {
   var apiKey = {
     key: '3e63059f-2e72-4fcb-ac0d-1663f970caaf'
   }
   request('GET', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apiKey.key)
     .then((res) => {
       var data = JSON.parse(res.target.response);
-      console.log('data.data: ', data.data);
+      // console.log('data.data: ', data.data);
       const currencyOne = currencyEl_one.value;
       const currencyTwo = currencyEl_two.value;
       for (var i = 0; i < data.data.length; i++) {
@@ -50,9 +78,29 @@ function fetchCMC() {
         // console.log('crypto symbol: ', symbol);
         // console.log('');
         if (currencyTwo === symbol) {
+          console.log("updating crypto rate...");
           rate.innerText = `1 ${currencyTwo} = ${price} ${currencyOne}`;
-          amountEl_one.value = (amountEl_two.value * price).toFixed(2);
-          amountEl_two.value = (amountEl_one.value * moneyRate).toFixed(2);
+          console.log("AFTER updating crypto rate...");
+          // TODO 
+          // Determine which input box was changed and then calculate accordingly
+          // Get the value of input box one and save to temp variable
+          // Get the value of input box two and save to temp variable
+          let countryValue = amountEl_one.value;
+          let cryptoValue = amountEl_two.value;
+          console.log("rate: " + price );
+          console.log("countryValue: " + countryValue );
+          console.log("cryptoValue: " + cryptoValue);
+          if(num == "one"){
+            console.log("Changing country currency, value: "+ amountEl_one.value);
+            //console.log("moneyRate: "+ moneyRate);
+            console.log("price: "+ price);
+            amountEl_two.value = (amountEl_one.value / price).toFixed(9);
+          }else {
+            amountEl_one.value = (amountEl_two.value * price).toFixed(2);
+          }
+          //amountEl_one.value = (amountEl_two.value * price).toFixed(2);
+          //amountEl_one.value = (amountEl_two.value * price).toFixed(2);
+          //amountEl_two.value = (amountEl_one.value * moneyRate).toFixed(2);
         }
       }
     }).catch((err) => err);
@@ -68,7 +116,7 @@ function fetchCMC() {
 }
 
 // get MT
-function fetchCryptoCompare() {
+function fetchMT() {
   const currencyOne = currencyEl_one.value;
   const currencyTwo = currencyEl_two.value;
   fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=MT&tsyms=USD,EUR')
